77d5499afef3a4cd4c33e848dad593af100cb74e
OpenWavesGit <150226555+OpenWavesGit@users.noreply.github.com>
2024-03-09 12:33:04 +0530

    batman is comming

--- aesfile/aesfile.go
+++ aesfile/aesfile.go
@@ -1,104 +1,58 @@
-package aesfile
+package aes
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
-	"io"
-	"os"
+    "crypto/aes"
+    "crypto/cipher"
+    "crypto/rand"
+    "encoding/base64"
+    "os"
 )
 
-// EncryptFile encrypts the contents of inputFile and writes the encrypted data to outputFile using AES encryption with the provided key.
-func EncryptFile(inputFile, outputFile string, key []byte) error {
-	// Open input file
-	inputFileHandle, err := os.Open(inputFile)
-	if err != nil {
-		return err
-	}
-	defer inputFileHandle.Close()
+const (
+    MySecret string = "abc&1*~#^2^#s0^=)^^7%b34"
+)
 
-	// Create output file
-	outputFileHandle, err := os.Create(outputFile)
-	if err != nil {
-		return err
-	}
-	defer outputFileHandle.Close()
-
-	// Generate a new AES cipher using the provided key
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return err
-	}
-
-	// Create a new GCM (Galois Counter Mode) cipher using AES block
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		return err
-	}
-
-	// Generate a random nonce
-	nonce := make([]byte, gcm.NonceSize())
-	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-		return err
-	}
-
-	// Write nonce to the beginning of the output file
-	if _, err = outputFileHandle.Write(nonce); err != nil {
-		return err
-	}
-
-	// Create a stream cipher using GCM mode with the nonce
-	stream := cipher.NewCTR(block, nonce)
-
-	// Create a writer that writes encrypted data to the output file
-	writer := &cipher.StreamWriter{S: stream, W: outputFileHandle}
-
-	// Copy data from input file to output file, encrypting as we go
-	if _, err = io.Copy(writer, inputFileHandle); err != nil {
-		return err
-	}
-
-	return nil
+func Encode(b []byte) string {
+    return base64.StdEncoding.EncodeToString(b)
 }
 
-// DecryptFile decrypts the contents of inputFile and writes the decrypted data to outputFile using AES decryption with the provided key.
-func DecryptFile(inputFile, outputFile string, key []byte) error {
-	// Open input file
-	inputFileHandle, err := os.Open(inputFile)
-	if err != nil {
-		return err
-	}
-	defer inputFileHandle.Close()
+func EncryptFile(inputFile, outputFile string) error {
+    // Read the file content
+    data, err := os.ReadFile(inputFile)
+    if err != nil {
+        return err
+    }
 
-	// Read the nonce from the beginning of the input file
-	nonce := make([]byte, 12) // Nonce size for GCM mode is 12 bytes
-	if _, err := io.ReadFull(inputFileHandle, nonce); err != nil {
-		return err
-	}
+    // Encrypt the file content
+    encryptedData, err := Encrypt(data)
+    if err != nil {
+        return err
+    }
 
-	// Create a new AES cipher using the provided key
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return err
-	}
+    // Write the encrypted content to a new file
+    err = os.WriteFile(outputFile, encryptedData, 0644)
+    if err != nil {
+        return err
+    }
 
-	// Create a stream cipher using GCM mode with the nonce
-	stream := cipher.NewCTR(block, nonce)
+    return nil
+}
 
-	// Create a reader that reads encrypted data from the input file
-	reader := &cipher.StreamReader{S: stream, R: inputFileHandle}
+func Encrypt(data []byte) ([]byte, error) {
+    block, err := aes.NewCipher([]byte(MySecret))
+    if err != nil {
+        return nil, err
+    }
 
-	// Create output file
-	outputFileHandle, err := os.Create(outputFile)
-	if err != nil {
-		return err
-	}
-	defer outputFileHandle.Close()
+    ciphertext := make([]byte, aes.BlockSize+len(data))
 
-	// Copy data from input file to output file, decrypting as we go
-	if _, err := io.Copy(outputFileHandle, reader); err != nil {
-		return err
-	}
+    iv := ciphertext[:aes.BlockSize]
+    if _, err := rand.Read(iv); err != nil {
+        return nil, err
+    }
 
-	return nil
+    stream := cipher.NewCFBEncrypter(block, iv)
+    stream.XORKeyStream(ciphertext[aes.BlockSize:], data)
+
+    return ciphertext, nil
 }
--- main.go
+++ main.go
@@ -5,35 +5,47 @@ import (
     "fmt"
     "github.com/OpenWavesGit/batman/batrunner" 
 	
-	"github.com/OpenWavesGit/batman/aesfile"
+//	"github.com/OpenWavesGit/batman/aesfile"
 )
 
 func main() {
 
 //
 //aes
+// inputFile := "input.txt"
+// 	encryptedFile := "output.enc"
+// 	decryptedFile := "output.txt"
+// 	key := []byte("0123456789abcdef0123456789abcdef") // 32 bytes for AES-256
+
+// 	// Encrypt file
+// 	if err := aesfile.EncryptFile(inputFile, encryptedFile, key); err != nil {
+// 		fmt.Println("Encryption Error:", err)
+// 		return
+// 	}
+// 	fmt.Println("File encrypted successfully.")
+
+// 	// Decrypt file
+// 	if err := aesfile.DecryptFile(encryptedFile, decryptedFile, key); err != nil {
+// 		fmt.Println("Decryption Error:", err)
+// 		return
+// 	}
+// 	fmt.Println("File decrypted successfully.")
+
 inputFile := "input.txt"
-	encryptedFile := "output.enc"
-	decryptedFile := "output.txt"
-	key := []byte("0123456789abcdef0123456789abcdef") // 32 bytes for AES-256
+outputFile := "encrypted_output.txt"
 
-	// Encrypt file
-	if err := aesfile.EncryptFile(inputFile, encryptedFile, key); err != nil {
-		fmt.Println("Encryption Error:", err)
-		return
-	}
-	fmt.Println("File encrypted successfully.")
+err := fileencrypt.EncryptFile(inputFile, outputFile)
+if err != nil {
+	fmt.Println("Error encrypting file:", err)
+	return
+}
 
-	// Decrypt file
-	if err := aesfile.DecryptFile(encryptedFile, decryptedFile, key); err != nil {
-		fmt.Println("Decryption Error:", err)
-		return
-	}
-	fmt.Println("File decrypted successfully.")
+fmt.Println("File encrypted successfully.")
+
 //aes
 //
     // Path to the batch file
-    batchFilePath := "path/to/your/batchfile.bat"
+    batchFilePath := "hello.bat"
 
     // Execute the batch file using the package
     output, err := batrunner.RunBatchFile(batchFilePath)
