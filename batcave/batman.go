dc65b722a0cdeb49a68f3a8c96888a1508f935e7
OpenWavesGit <150226555+OpenWavesGit@users.noreply.github.com>
2024-03-09 18:23:08 +0530

    batman is comming

--- batcave/batman.go
+++ batcave/batman.go
@@ -12,19 +12,38 @@ import (
 	"github.com/OpenWavesGit/batman/uniquekey"
 )
 
-func keygen() {
+func keygen(kname string) {
 	keyLength := 10
-	filename := "generated_key.txt"
-	err := uniquekey.GenerateKey(keyLength, filename)
+	filename := kname+".txt"
+	err := uniquekey.GenerateKey(keyLength, kname ,filename)
 	if err != nil {
 		fmt.Println("Error generating key:", err)
 		return
 	}
-	fmt.Println("Key generated and stored in:", filename)
+	
+}
+func fold(nam string) {
+    // Name of the folder you want to create
+    folderName := nam
+
+    // Use "." to represent the current directory
+    path := "."
+
+    // Concatenate folder name and path to get the full directory path
+    folderPath := path + "/" + folderName
+
+    // Use os.MkdirAll to create the folder and any necessary parent directories
+    err := os.MkdirAll(folderPath, os.ModePerm)
+    if err != nil {
+        fmt.Println("Error creating folder:", err)
+        return
+    }
+
+    fmt.Println("Folder created successfully at", folderPath)
 }
 
 func main() {
-
+	var intt  int
 	//
 	reader := bufio.NewReader(os.Stdin)
 
@@ -32,9 +51,18 @@ func main() {
 
 	fmt.Printf("Hello,  Welcome to the Ever Automation Batman App!\n")
 	//
-
+	fmt.Println("\n enter the company name")
+	cmpname := readerss.Readd()
+	fmt.Printf("Enter the Location of the Batch file!\n")
 	inputFile := readerss.Readd()
 	encryptedFile := "encrypted.enc"
+	fmt.Println("Enter the Total number of keys for the company")
+	fmt.Scanln(&intt)
+	fold(cmpname)
+	for i := 1; i <= intt; i++ {
+		keygen(cmpname)
+	}
+	fmt.Println("Key generated and stored in:",cmpname)
 	//	decryptedFile := "hello.bat"
 
 	// Encrypt the input file
